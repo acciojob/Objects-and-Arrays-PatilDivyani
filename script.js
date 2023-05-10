f619e05d4700bf3b36002ae4d9562ded4cff283f
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-10 13:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team = players;
+/var team1 = players;
+var team1 = {}; // the new empty object
+
+// let's copy all user properties into it
+for (let key in players) {
+  team1[key] = players[key];
+}
+var cap1 = Object.assign({}, players);
 
 window.players = players;
 window.person = person;
